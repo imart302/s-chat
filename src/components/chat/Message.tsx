1e1a4a1235d368668dbe09ac4bfc8984da1ad665
Ivan Martinez <imartinezchin@gmail.com>
2023-05-16 17:50:19 -0600

    Changing message view and add date of the message

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from '@/styles/Chat.module.scss';
 
 interface MessageProps {
   user: string;
@@ -7,8 +8,11 @@ interface MessageProps {
 }
 
 export const Message: React.FC<MessageProps> = (props) => {
-  return <div className='d-flex' style={{width: '100%'}}>
-    <span style={{color: "#4600c7", whiteSpace: 'pre'}}>{props.user+': '}</span>
-    <p style={{wordWrap: 'break-word', width: '80%'}}>{" "}{props.message}</p>
+  return <div className={styles.messageContainer}>
+    <div className={styles.innerMessageContainer}>
+      <span style={{color: "#4600c7", whiteSpace: 'pre'}}>{props.user+': '}</span>
+      <p style={{wordWrap: 'break-word', width: '80%'}}>{" "}{props.message}</p>
+    </div>
+    <p style={{fontSize: '10px', marginLeft: '5px', marginBottom: '5px'}}>{props.date.toLocaleDateString()}{" "}{props.date.toLocaleTimeString()}</p>
   </div>;
 };
