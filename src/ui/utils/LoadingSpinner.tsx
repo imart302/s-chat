aa7dc85386f11f07d88802dbd7a4d778a43cbd0b
Ivan Martinez <imartinezchin@gmail.com>
2023-05-17 17:52:17 -0600

    Adding style in loading spinner

--- src/ui/utils/LoadingSpinner.tsx
+++ src/ui/utils/LoadingSpinner.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export const LoadingSpinner = () => {
   return (
-    <h1>Loading ...</h1>
-  )
-}
+    <div className="d-flex w-100 h-100 align-items-center justify-content-center">
+      <div className="spinner-border text-primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
